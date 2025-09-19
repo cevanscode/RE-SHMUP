2da5bf1aba0a696b64b8b0af05c31faa01b5922d
cevanscode <c.hunter.evans@gmail.com>
2025-09-18 22:26:30 -0500

    Added Class Library MonoGameLibrary
    Initial work on scenes

--- RE_SHMUPGame.cs
+++ RE_SHMUPGame.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using MonoGameLibrary;
 
 namespace RE_SHMUP
 {
---
+++ Scenes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace RE_SHMUP
+{
+    public abstract class Scenes : IDisposable
+    {
+        /// <summary>
+        /// Gets content manager to load a scene's assets
+        /// </summary>
+        protected ContentManager Content { get; }
+
+        /// <summary>
+        /// Tells if a scene is disposed
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+        
+        public Scenes()
+        {
+            Content = new ContentManager(Content.ServiceProvider);
+
+            Content.RootDirectory = Content.RootDirectory;
+        }
+
+        ~Scenes() => Dispose(false);
+
+        public virtual void Initialize()
+        {
+            LoadContent();
+        }
+
+        public virtual void LoadContent() { }
+
+        public virtual void UnloadContent() { }
+
+        public virtual void Update(GameTime gameTime) { }
+
+        public virtual void Draw(GameTime gameTime) { }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (IsDisposed) { return; }
+
+            if (disposing)
+            {
+                UnloadContent();
+                Content.Dispose();
+            }
+        }
+    }
+}
