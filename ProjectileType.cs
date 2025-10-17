2045cf295e6c46bbf325e7c901e35b9d04e25894
cevanscode <c.hunter.evans@gmail.com>
2025-10-17 00:22:03 -0500

    Added projectile type enum (important tool for later)

---
+++ ProjectileType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RE_SHMUP
+{
+    /// <summary>
+    /// Enumeration of the two projectile types in the game. Determines what can be absorbed.
+    /// </summary>
+    public enum ProjectileType
+    {
+        Beam,
+        Kinetic
+    }
+}
